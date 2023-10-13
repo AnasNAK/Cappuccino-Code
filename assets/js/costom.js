7eb0a5027c76b31248ed9a03cc56a5ab46cb568e
AnasNAK <anasc00ding@gmail.com>
2023-10-13 22:05:18 +0100

    add functions

---
+++ assets/js/costom.js
@@ -0,0 +1,44 @@
+document.querySelector('a[href="#scrollTarget"]').addEventListener('click', function(e) {
+    e.preventDefault();  
+    document.querySelector('#scrollTarget').scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.filter-button');
+    const containers = document.querySelectorAll('.container-boxes');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            const category = this.getAttribute('data-category');
+
+            containers.forEach(container => {
+                container.style.opacity = 0;
+                container.style.display = 'none';
+            });
+
+            if (category === 'all') {
+                containers.forEach(container => {
+                    container.style.display = 'block';
+                    container.style.cssText ='display: flex;justify-content: space-evenly;';
+                    setTimeout(() => {
+                        container.style.opacity = 1; 
+                    }, 50);
+                });
+            } else {
+                const selectedContainer = document.querySelector(`.container-boxes[data-category="${category}"]`);
+                if (selectedContainer) {
+                    selectedContainer.style.display = 'block';
+                    selectedContainer.style.cssText ='display: flex;justify-content: space-evenly;';
+                    setTimeout(() => {
+                        selectedContainer.style.opacity = 1; 
+                    }, 50);
+                }
+                
+            }
+        });
+    });
+});
